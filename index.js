30eb6067da27b3951b25b4468f677b97864a4ec5
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-03-11 09:02:50 -0500

    finished first task

--- index.js
+++ index.js
@@ -28,11 +28,14 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  the difference between the them is where the variablee is being called from. funtion 1 uses a variable that is scoped to the variable and function 2 is using a global variable.
   
   2. Which of the two uses a closure? How can you tell?
+  function 1 is using close because its calling on a varriable nested within the function
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     the first examble would be better suited if you didn't want any other way to access that variable but if that variable may be used in other ways its best to use example 2
 */
 
 // counter1 code
